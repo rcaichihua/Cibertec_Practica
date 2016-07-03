c4951ad247b4c258f9a4f8145db6e3c65ef1a8c4
CaichihuaVergaraRoger <rcaichihua@gmail.com>
2016-07-03 15:33:04 -0500

    Actualizacion

---
+++ WebDeveloper.DataAccess/WebContextDb.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using WebDeveloper.Model;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace WebDeveloper.DataAccess
+{
+    public class WebContextDb : DbContext
+    {
+        public WebContextDb() : base("name=WebDeveloperConnectionString")
+        {
+            //Database.SetInitializer(new WebDeveloperInitializer());
+        }
+        public DbSet<Categories> categories { get; set; }
+        public DbSet<Customers> customers { get; set; }
+        public DbSet<Employees> employees { get; set; }
+        public DbSet<Orders> orders { get; set; }
+        public DbSet<Products> products { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            //base.OnModelCreating(modelBuilder);
+        }
+    }
+}
